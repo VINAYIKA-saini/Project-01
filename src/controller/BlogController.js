376f059507554e41f8a8c661b83ed988cf99acc1
sabiha <you@example.com>
2022-09-05 18:38:09 +0530

    not done

--- src/controller/BlogController.js
+++ src/controller/BlogController.js
@@ -1,19 +1,56 @@
+const { count } = require("console")
+const blogModel = require("../models/blogModel")
 
-   const BlogModel= require("..models/BlogModel")
 
-   const createBlog= async function (res,req)
-{ 
-   let data = req.body
-   let savedData=await BlogModel.create(data)
-   res.send({msg:savedData})
-}
-
-   const getBlogdata= async function (req ,res)
-
+const createblog = async function (req, res) 
 {
-    let allBlog=await BookModel.find({})
-    res.send({msg:allBlog})
+    try 
+    {
+        let data = req.body
+        console.log(data)
+        if ( Object.keys(data).length != 0) {
+            let savedData = await blogModel.create(data)
+            res.status(201).send({ msg: savedData })
+        }
+        else res.status(400).send({ msg: "BAD REQUEST"})
+    }
+    catch (err) 
+    {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
 }
 
-module.export.createBlog
-module.exo
+const getblogData = async function (req, res) 
+{
+    let allblog = await blogModel.find({ authorName: "" })
+    console.log(allblog)
+    if (allblog.length > 0) res.send({ msg: allblog, condition: true })
+    else res.send({ msg: "No blogs found", condition: false })
+}
+
+
+const updateblog = async function (req, res) 
+{
+    let data = req.body 
+    
+    let allblog = await blogModel.findOneAndUpdate(
+        { authorName: "ABC" }, 
+        { $set: data }, 
+        { new: true, upsert: true } ,
+    )
+
+    res.send({msg: allblog})
+}
+
+
+
+
+
+
+
+
+
+module.exports.createblog = createblog
+module.exports.getblogData = getblogData
+module.exports.updateblog = updateblog
