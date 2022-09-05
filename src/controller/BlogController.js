fca28421ce86e53bf76fedc9630ec46a4d7d53da
sabiha <you@example.com>
2022-09-05 16:43:48 +0530

    not done

---
+++ src/controller/BlogController.js
@@ -0,0 +1,19 @@
+
+   const BlogModel= require("..models/BlogModel")
+
+   const createBlog= async function (res,req)
+{ 
+   let data = req.body
+   let savedData=await BlogModel.create(data)
+   res.send({msg:savedData})
+}
+
+   const getBlogdata= async function (req ,res)
+
+{
+    let allBlog=await BookModel.find({})
+    res.send({msg:allBlog})
+}
+
+module.export.createBlog
+module.exo
