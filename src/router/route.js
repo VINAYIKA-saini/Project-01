06c297c9405c662f94b3c0839acdfde73d5e7888
sabiha <you@example.com>
2022-09-06 01:01:25 +0530

    done

--- src/router/route.js
+++ src/router/route.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const AuthorController = require("../controllers/authorController");
-// const BlogController = require("../controllers/blogController");
-
 const blogController = require("../controllers/blogController");
 
-router.post("/authors",AuthorController.createAuthor);
-router.post("/blog",blogController.createblog)//",/controllers/blogController");
 
+
+router.post("/authors",AuthorController.createAuthor);
+router.post("/blog",blogController.createBlogs)
 module.exports = router;
